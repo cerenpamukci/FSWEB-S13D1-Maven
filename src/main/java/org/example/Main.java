dc5c2e8b5071c738250cf44d6e90b9658b614f1f
Ceren Güney <102367342+cerenpamukci@users.noreply.github.com>
2024-09-16 21:57:33 +0300

    tamamlanan proje

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,36 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if (clock<0 || clock>23 || !isBarking) {
+            return false;
+        }
+        return clock >=20 || clock<8;
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+       return ((firstAge>13 && firstAge<=19)|| (secondAge>13 && secondAge<=19)|| (thirdAge>13 && thirdAge<=19) ) ;
+
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+     int lowestTemp = 25;
+     int highestTemp = isSummer ? 45 : 35;
+     return temp< highestTemp && temp>lowestTemp;
     }
 
     public static double area(double width, double height) {
-       
+       if (width<0 || height<0) {
+           System.out.println("width veya height sıfırdan küçük olamaz!! Width: " + width + "Height: " + height);
+           return -1;
+       }
+       return width*height;
     }
 
     public static double area(double radius) {
-       
+       if (radius<0) {
+           System.out.println("radius sıfırdan küçük olamaz! Radius: " +radius);
+           return -1;
+       }
+       return Math.PI*Math.pow(radius,2);
     }
 }
